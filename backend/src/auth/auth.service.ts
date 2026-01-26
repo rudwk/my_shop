d5317c84b882445d0d2beb77029807c918823e0b
rudwk <tlsruwk12345@gmail.com>
2026-01-26 09:31:54 +0900

    refresh token으로 accsess token 재발급

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import Redis from 'ioredis';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    @InjectRedis() private readonly redis: Redis,
+  ){}
+
+  async generateAccsessToken(refreshToken: string) {
+    let payload;
+    try {
+      payload = this.jwtService.verify(refreshToken, { secret: process.env.JWT_REFRESH_SECRET });
+    } catch (e) {
+      throw new NotFoundException('만료된 refresh 토큰입니다');
+    }
+    const storedToken = await this.redis.get(`refresh:${payload.sub}`);
+    
+    if (storedToken !== refreshToken) {
+      throw new UnauthorizedException('탈취된 refresh token');
+    }
+
+    const newAccsess = this.jwtService.sign({ sub: payload.sub, nickname: payload.nickname }, { secret: process.env.JWT_ACCSESS_SECRET, expiresIn: '30m' });
+    return { accsessToken: newAccsess };
+  }
 }
